87c4eec3d4d6f07de4e33027239f1ad87578301b
Gabor Hornyak <gabhornyak@gmail.com>
2022-05-23 18:01:17 +0200

    feat: basic graphql setup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
 import config from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/HelloResolver";
 
 const main = async () => {
   const orm = await MikroORM.init(config);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: "First Post" });
-  await orm.em.persistAndFlush(post);
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver]
+    })
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log("App is listening on port 4000.");
+  });
 };
 
 main();
