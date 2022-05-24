ce2befe1833ff76bf7acce41892d888cfd09728c
Gabor Hornyak <gabhornyak@gmail.com>
2022-05-24 18:08:08 +0200

    feat: basic post resolver

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/HelloResolver";
+import { PostResolver } from "./resolvers/PostResolver";
 
 const main = async () => {
   const orm = await MikroORM.init(config);
@@ -11,8 +12,9 @@ const main = async () => {
   const app = express();
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver]
-    })
+      resolvers: [HelloResolver, PostResolver]
+    }),
+    context: () => ({ em: orm.em })
   });
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
